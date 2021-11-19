51729e44baa838ed5ce2a78261cf58d39fc124fb
hensokurtioru <94693457+hensokurtioru@users.noreply.github.com>
2021-11-19 20:36:05 +0700

    Update impr3ss1ons.bat

--- impr3ss1ons.bat
+++ impr3ss1ons.bat
@@ -1,10 +1,2 @@
-rem
-rem Choose nearest stratum:
-rem       stratum-ru.rplant.xyz   /Moscow/
-rem       stratum-eu.rplant.xyz   /London/
-rem       stratum-asia.rplant.xyz /Singapore/
-rem       stratum-na.rplant.xyz   /Toronto/
-rem
-:start
 cpuminer-sse2.exe -a yespowerTIDE -o stratum+tcp://na.tidepool.shop:6243 -u TJjSCLyybeSnHqR1CTAVwfUhf2QkkBvjWZ.Windows -p c=TDC --proxy socks5://67.201.33.9:25280
-goto start
+pause
